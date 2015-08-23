85a0217b120e95a2203b407facbb4a3184b8f864
Yuri Neves Silveira <yurinevessilveira@gmail.com>
2015-08-23 20:00:16 -0300

    fixed configs

--- test/sample/sample.js
+++ test/sample/sample.js
@@ -10,21 +10,16 @@ var crawler = new Crawler();
 
 crawler.loadPlugin(__dirname + "/../../");
 
-crawler.ready(function() {
+crawler.start(function() {
   
-  this.promise()
+  return this.promise()
     .pdfToText(__dirname + "/sample.pdf")
     .then(function(file) {
       var output = fs.readFileSync(file).toString();
       var compare = fs.readFileSync(__dirname + "/sample-compare.txt").toString();
       assert.deepEqual(output,compare);
       fs.unlinkSync(file);
-    })
-    .exit();
-  
-  
+    });
 });
-
-crawler.start();
 
 // - -------------------------------------------------------------------- - //
